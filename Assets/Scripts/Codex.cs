4ae99c19f5a038d5151e082c2e74967528c1cec7
npreis <nicholaspreis@gmail.com>
2023-04-13 17:04:42 -0400

    Added some codex code.

---
+++ Assets/Scripts/Codex.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Codex : MonoBehaviour
+{
+    public bool isUnlocked;
+    public string entryTitle;
+    public string entryDesc;
+    public Sprite entrySprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/CodexManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CodexManager : MonoBehaviour
+{
+    public List<Codex> codices;
+    public static CodexManager instance;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
