4ca7bc722d438835c6e1df2ab4f77d306827c112
npreis <nicholaspreis@gmail.com>
2023-06-28 16:58:04 -0400

    Fixed an hp bug.

--- Assets/Scripts/UIScript.cs
+++ Assets/Scripts/UIScript.cs
@@ -68,6 +68,10 @@ public class UIScript : MonoBehaviour
 
     void UpdateHealth()
     {
+        if(manager.leader.currHealth > leaderHP.maxValue)
+        {
+            manager.leader.currHealth = manager.leader.GetMaxHealth();
+        }
         leaderHP.value = manager.leader.currHealth;
         leaderHP.maxValue = manager.leader.GetMaxHealth();
         leaderHP.fillRect.gameObject.SetActive(leaderHP.value > 0);
